0e4316ba42bcaa7ce90970a453c22b65159a2c2b
random-logic <39440726+random-logic@users.noreply.github.com>
2024-03-09 01:01:33 -0800

    progress

--- Project2/to_google_doc.R
+++ Project2/to_google_doc.R
@@ -1,2 +1,2 @@
-input <- "R Markdown code.Rmd"
+input <- "STA_106_Project_2.Rmd"
 rmarkdown::render(input, rmarkdown::word_document())
--- render.R
+++ render.R
@@ -1,2 +1,3 @@
-input <- "Project2-Trans.Rmd"
-rmarkdown::render(input, output_format = "html_document")
+# pdf does not format well, just make it a word file
+input <- "HW6.Rmd"
+rmarkdown::render(input, output_format = "word_document")
