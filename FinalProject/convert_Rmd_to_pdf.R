7411c2f20577ac1de7340734904442752a848a98
random-logic <39440726+random-logic@users.noreply.github.com>
2024-03-18 01:18:46 -0700

    finish

--- FinalProject/convert_Rmd_to_pdf.R
+++ FinalProject/convert_Rmd_to_pdf.R
@@ -1,4 +1,4 @@
 # This R code converts an Rmd file to a pdf file
 # For example, if input.Rmd is in the same path as this file:
-input <- "diagnostics.Rmd" # You would change this
+input <- "analysis.Rmd" # You would change this
 rmarkdown::render(input, output_format = "pdf_document")
--- FinalProject/missing_libraries.R
+++ FinalProject/missing_libraries.R
@@ -1,4 +1,4 @@
 # Use this file to install libraries
 # If you are given an error about a missing dependency
-lib_name <- "vscDebugger" # change this
+lib_name <- "readr" # change this
 install.packages(lib_name)
