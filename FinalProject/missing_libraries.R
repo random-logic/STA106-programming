42dc68176662817a9c8ada8e97907f79dcc2ed26
random-logic <39440726+random-logic@users.noreply.github.com>
2024-03-28 17:23:20 -0700

    upload

--- FinalProject/missing_libraries.R
+++ FinalProject/missing_libraries.R
@@ -1,4 +1,4 @@
 # Use this file to install libraries
 # If you are given an error about a missing dependency
-lib_name <- "readr" # change this
+lib_name <- "car" # change this
 install.packages(lib_name)
